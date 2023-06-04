9fc695e55599cf41fe67444c98c8d918d90803d2
michaeln014 <michaelnguyen1475065@gmail.com>
2023-06-04 16:05:26 -0700

    buggy code set, cause outofbound

--- ListExamples.java
+++ ListExamples.java
@@ -10,9 +10,9 @@ class ListExamples {
   // the same order they appeared in the input list;
   static List<String> filter(List<String> list, StringChecker sc) {
     List<String> result = new ArrayList<>();
-    for(int i = 0; i <= list.size(); i++) {
+    for(int i = 0; i <= list.size(); i+=10) {
       if(sc.checkString(list.get(i))) {
-        result.add(0, list.get(i));
+        result.add(list.get(i));
       }
     }
     return result;

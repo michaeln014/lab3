3e2aed77b7c2c876440cbdb0fd8b30bd438dbd35
michaeln014 <michaelnguyen1475065@gmail.com>
2023-06-04 15:35:35 -0700

    some commit

--- ArrayTests.java
+++ ArrayTests.java
@@ -10,9 +10,24 @@ public class ArrayTests {
 	}
 
 
-  @Test
-  public void testReversed() {
-    int[] input1 = { };
-    assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
-  }
+  // @Test
+  // public void testReversed() {
+  //   int[] input1 = { };
+  //   assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+  // }
+
+
+  // @Test
+  // public void testReversed2() {
+  //   int[] input1 = {1,2,3};
+  //   assertArrayEquals(new int[]{3,2,1}, ArrayExamples.reversed(input1));
+  // }
+
+  
+  // @Test
+  // public void testReversedInPlace2() {
+  //   int[] input1 = {1,2,3,0};
+  //   ArrayExamples.reverseInPlace(input1);
+  //   assertArrayEquals(new int[]{0,3,2,1}, input1);
+  // }
 }
